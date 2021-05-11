b9015027b2b6db54794b256b3c1e945a66cf8674
Thinkpad <2128903798@qq.com>
2021-05-11 21:44:48 +0800

    Initial commit

---
+++ src/Example.java
@@ -0,0 +1,10 @@
+public class Example {
+    public static void main(String args[]){
+        Circle a=new Circle();
+        Rectangle b=new Rectangle();
+        Square c=new Square();
+        a.getAera();
+        b.getAera();
+        c.getAera();
+    }
+}
